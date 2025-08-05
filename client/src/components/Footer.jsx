e4aca56659144c14b95575217f5162f4264af659
Shivam Sharma <132919442+SharmaShivam20@users.noreply.github.com>
2025-08-06 00:23:49 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { assets } from '../assets/assets'
+import logo from './logo.svg'
 
 const Footer = () => {
   return (
     <div className='flex items-center justify-between gap-4 py-3 mt-20'>
         
-        <img src={assets.logo} alt="" width={150} />
+        <img src={logo} alt="" width={150} />
 
         <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>Copyright @SetientBeing | All right reserved.</p>
 
